5bf97684652fea21d8263b2a7bdd904b24bbb5c3
Geert-Jan Huizing <huizing@ens.fr>
2022-05-17 23:02:52 +0200

    documentation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wsingular',
-      version='0.0.1',
+      version='0.1.0',
       description='Wasserstein Singular Vectors',
       author='Geert-Jan Huizing',
       author_email='huizing@ens.fr',
