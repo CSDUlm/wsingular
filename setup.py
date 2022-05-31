e85452692ab1b6179cdd345beb2e7037517f3e8f
Geert-Jan Huizing <huizing@ens.fr>
2022-05-31 14:22:48 +0200

    change patch version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="wsingular",
-    version="0.1.3",
+    version="0.1.4",
     description="Wasserstein Singular Vectors",
     long_description=long_description,
     long_description_content_type="text/markdown",
