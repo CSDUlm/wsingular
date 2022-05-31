cdbfe7f507b61ed1b050776505f83d65bbb71b17
Geert-Jan Huizing <huizing@ens.fr>
2022-05-31 10:57:50 +0200

    add install_requires

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="wsingular",
-    version="0.1.1",
+    version="0.1.2",
     description="Wasserstein Singular Vectors",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,4 +15,18 @@ setup(
     url="https://github.com/gjhuizing/wsingular",
     author_email="huizing@ens.fr",
     packages=["wsingular"],
+    install_requires=[
+        "torch",
+        "protobuf<=3.20.1",
+        "tensorboard",
+        "pandas",
+        "sklearn",
+        "seaborn",
+        "matplotlib",
+        "scipy",
+        "tqdm",
+        "numpy",
+        "pot>=0.8",
+        "scikit-network",
+    ],
 )
